471bb79a4b755856b54804be8bbec50cda3eb052
Jim Rhotert <rhotert@stud.uni-hannover.de>
2022-12-05 12:47:39 +0100

    hbl data pipeline

---
+++ scripts/hbl_data.py
@@ -0,0 +1,166 @@
+"""
+This code regarding the synchronization of positions and video frames 
+is adapted from the single-match-example
+
+    -> /nfs/data/mm4spa/mm_hbl/scripts/sync/single_match_example.py
+
+This script is intended to synchronize position and video data and their respective annotations.
+"""
+
+import sys
+sys.path.append("/nfs/data/mm4spa/mm_hbl/scripts/sync/")
+
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import json
+import pickle as pkl
+from tqdm import tqdm
+
+import sync
+from kinexon_reader import read_kinexon_file
+
+DATA_SOURCE = Path("/nfs/data/mm4spa/mm_hbl/hbl_19-20")
+
+EVENTS_PATH = Path("/nfs/home/rhotertj/datasets/hbl/events")
+POSITIONS_PATH = Path("/nfs/home/rhotertj/datasets/hbl/positions")
+FRAMES_PATH = Path("/nfs/home/rhotertj/datasets/hbl/frames")
+META_PATH = Path("/nfs/home/rhotertj/datasets/hbl/")
+
+
+def read_all_matches_meta():
+    # base mapping to video and position data
+    index_col = "match_id"
+    usecols = [index_col, "raw_pos_knx", "raw_video"]
+    df_matches = pd.read_csv(
+        DATA_SOURCE / "raw_mapping.csv", index_col=index_col, usecols=usecols
+    )
+
+    # offsets necessary for sync
+    usecols = [index_col, "v_h1_start", "v_h1_end", "v_h2_start", "v_h2_end", "p_h1", "p_h2"]
+    df_anno = pd.read_csv(DATA_SOURCE / "raw_base_annotations.csv", index_col=index_col, usecols=usecols)
+    df = df_matches.join(df_anno)
+    df = df.dropna(axis=0, subset=["v_h1_start", "p_h1"])
+
+    # necessary video information
+    usecols = [index_col, "duration", "avg_frame_rate"]
+    df_video = pd.read_csv(DATA_SOURCE / "raw_video.csv", index_col=index_col, usecols=usecols)
+    df = df.join(df_video)
+    return df
+
+def read_sync_sportradar_data(fname, meta_info):
+    with open(fname) as fr:
+        # dict_keys(['generated_at', 'sport_event', 'sport_event_status', 'statistics', 'timeline'])
+        json_events = json.load(fr)
+    df_events = pd.DataFrame.from_records(json_events["timeline"]).sort_values("time")
+
+    df_events["localtime_ms"] = pd.to_datetime(df_events["time"], unit="ns").view(
+        np.int64
+    ) / int(1e6)
+
+    df_events = df_events.loc[
+        df_events["type"].isin(
+            ["period_start", "break_start", "match_started", "match_ended", "shot_blocked"]
+        )
+    ]
+    df_events = df_events[["type", "time", "localtime_ms"]]
+    df_events["t_v"] = sync.localtime2video(
+        t_w=pd.to_datetime(df_events["localtime_ms"], unit="ms"),
+        t_null=pd.to_datetime(meta_info.t_null, unit="ms"),
+        v_h1_start=meta_info.v_h1_start,
+        v_h2_start=meta_info.v_h2_start,
+        offset_p_h1=meta_info.p_h1,
+        offset_p_h2=meta_info.p_h2,
+    ) # aligned video timestamp t_v in [s]
+    return df_events
+
+def align_position_to_video(pos_sets, meta_info):
+    # Resample pos to video frame rate by interpolation
+    resampling_factor = meta_info.avg_frame_rate / meta_info.avg_frame_rate_pos
+    pos_sets = [sync.resample(xyz, resampling_factor) for xyz in pos_sets]
+    # print(f"After resampling to {meta_info.avg_frame_rate=}fps", [x.shape for x in pos_sets])
+
+    # Align position data to video
+    pos_sets = [
+        sync.pos2video(
+            pos,
+            frames_video=int(round(meta_info.duration * meta_info.avg_frame_rate)),
+            v_h1_start=int(round(meta_info.v_h1_start * meta_info.avg_frame_rate)),
+            v_h1_end=int(round(meta_info.v_h1_end * meta_info.avg_frame_rate)),
+            v_h2_start=int(round(meta_info.v_h2_start * meta_info.avg_frame_rate)),
+            v_h2_end=int(round(meta_info.v_h2_end * meta_info.avg_frame_rate)),
+            offset_p_h1=int(round(meta_info.p_h1 * meta_info.avg_frame_rate)),
+            offset_p_h2=int(round(meta_info.p_h2 * meta_info.avg_frame_rate)),
+            constant_values=0.0,
+            replace_nan=0.0,
+        )
+        for pos in pos_sets
+    ]
+    # print(f"After alignment with video num_frames_video={int(round(meta_info.duration * meta_info.avg_frame_rate))}:", [x.shape for x in pos_sets])
+    return pos_sets
+
+def main():
+
+    # Get annotation filenames for given matches
+    event2json_df = pd.read_csv("/nfs/data/mm4spa/mm_hbl/hbl_19-20/events_its.csv", index_col="match_id")
+    match_id2event_json = event2json_df.to_dict()["events_its"]
+
+    # Read meta info for all matches, including their video and position files
+    meta_df = read_all_matches_meta()
+    new_meta_df = meta_df.copy(deep=True)
+    new_meta_df["n_frames"] = round(meta_df["duration"] * meta_df["avg_frame_rate"])
+    new_meta_df["frames_path"] = ""
+    new_meta_df["events_path"] = ""
+    new_meta_df["positions_path"] = ""
+
+    for match_number in tqdm(range(len(meta_df))):
+        # Prepare meta info for selected match
+        meta_info = sync.SyncInformation(
+            **{
+                **meta_df.iloc[match_number].to_dict(),
+                "match_id_min": meta_df.index[match_number].replace("sr:sport_event:", ""),
+            },
+            avg_frame_rate_pos=None,
+            t_null=None,
+        )
+        
+        # Read position data (frame rate != video frame rate) and resample positions to video frame rate
+        # Then, align position data such that each frame matches one position
+        # print("Reading info for", meta_info.match_id_min)
+        pos_sets, t_start, avg_frame_rate_pos, timestamps = \
+            read_kinexon_file(
+            DATA_SOURCE / "raw_pos_knx" / meta_info.raw_pos_knx
+            )
+
+        meta_info.t_null = t_start
+        meta_info.avg_frame_rate_pos = avg_frame_rate_pos
+        # print(f"{meta_info.avg_frame_rate_pos=}fps", [x.shape for x in pos_sets])
+
+        pos_sets = align_position_to_video(pos_sets, meta_info)
+
+        pos_available = sync.pos_available(pos_sets[0], pos_sets[1], value_check="zeros")
+        pos_sets.append(pos_available)
+
+        event_file = match_id2event_json[f"sr:sport_event:{meta_info.match_id_min}"]
+        # print("Read", DATA_SOURCE / "events_its" / event_file)
+        events_df = pd.read_json(DATA_SOURCE / "events_its" / event_file, lines=True)
+
+        events_path = EVENTS_PATH / (meta_info.match_id_min + ".csv")
+        events_df.to_csv(events_path)
+        new_meta_df.loc[meta_df.index[match_number], "events_path"] = events_path
+
+        positions_path = POSITIONS_PATH / (meta_info.match_id_min + ".npy")
+        with open(positions_path, "wb+") as f:
+            pkl.dump(pos_sets, f)
+        new_meta_df.loc[meta_df.index[match_number], "positions_path"] = positions_path
+
+        new_meta_df.loc[meta_df.index[match_number], "frames_path"] = FRAMES_PATH / f"{event_file[:6]}_30.mp4.d"
+
+    new_meta_df.to_csv(META_PATH  / "meta.csv")
+    print(new_meta_df)           
+    # TODO: Q: What is position available?
+    # TODO: More comments and documentation at the top :)
+
+
+if __name__== "__main__":
+    main()
---
+++ src/data.py
@@ -0,0 +1,188 @@
+
+from torch.utils.data import Dataset
+import numpy as np
+import pickle as pkl
+import pandas as pd
+from pathlib import Path
+import cv2
+
+# TODO: How to elegantly split wrt matches?
+# TODO: Prepare different representation for positions
+        # -> On the fly plotting might be too slow
+
+class HandballSyncedDataset(Dataset):
+
+    def __init__(self, meta_path : str, seq_len : int = 8, load_frames : bool = True):
+        super(HandballSyncedDataset).__init__()
+
+        self.seq_len = seq_len
+        self.seq_half = seq_len // 2
+        self.load_frames = load_frames
+
+        # might be useful to pass information from pytorch lightning config about dataset
+        self.meta = {}
+
+        print(f"Read {meta_path}...")
+        meta_df = pd.read_csv(meta_path)
+        self.index_tracker = [0]
+        self.idx_to_frame_number = []
+        self.frame_paths = []
+        self.event_dfs = []
+        self.position_arrays = []
+        for _, row in meta_df.iterrows():
+            self.frame_paths.append(row["frames_path"])
+            self.event_dfs.append(
+                pd.read_csv(row["events_path"], index_col="t_start")
+            )
+            with open(row["positions_path"], "rb") as f:
+                self.position_arrays.append(
+                    pkl.load(f)
+                )
+        
+        # NOTE: All arrays and paths are indexed based on frame number.
+        # We need to create a mapping to ensure availability of position data 
+        for *_, availability in self.position_arrays:
+            kernel = np.ones(self.seq_len)
+            available_windows_cvn = np.convolve(availability, kernel, mode="valid")
+            available_windows = np.where(available_windows_cvn == self.seq_len)[0]
+            self.idx_to_frame_number.append(available_windows)
+            self.index_tracker.append(int(self.index_tracker[-1] + len(available_windows)))
+
+        print("Tracker", self.index_tracker)
+
+
+    def __len__(self):
+        # Sliding window approach needs half a sequence as padding
+        return self.index_tracker[-1] - self.seq_len
+
+    def __getitem__(self, idx):
+        """This method returns 
+            - stacked frames (RGB)
+            - corresponding positional data for players and ball
+            - the corresponding event label
+            - the annotated event position in the frame stack.
+
+        Note that the provided index is valid over the whole dataset. It can not be treated as a frame number or index for a given match.
+        It first needs to be matched against a match and a frame number, based on the availability of positional data.
+
+        Args:
+            idx (int): Index of data to be retrieved.
+
+        Returns:
+            dict: A dict containing frames, positions, label and label_offset.
+        """
+        # return frames (FrameSeqDataset from mmaction github fork), positions (start with array repr), label (turn dict into), action frame offset and just-one-modality option
+
+        # Get correct match based on idx (match_number) and idx with respect to match and availability (frame_idx) from idx with respect to dataset (param: idx)
+        # Add half sequence length to index to avoid underflowing dataset idx < seq_len
+        match_number, frame_idx = get_index_offset(self.index_tracker, self.idx_to_frame_number, idx + self.seq_half)
+        
+        frame_base_path = Path(self.frame_paths[match_number])
+        events = self.event_dfs[match_number]
+        frames = []
+        
+        label = 2 # Default to 'background' action
+        label_offset = 0 # Which frame of the window is portraying the action
+        for window_idx in range(frame_idx - self.seq_half, frame_idx + self.seq_half):
+            if window_idx in events.index:
+                label = events.loc[window_idx].labels
+                label_offset = (window_idx - frame_idx) + self.seq_half
+            if self.load_frames:
+                frame_path = str(frame_base_path / f"{str(window_idx).rjust(6, '0')}.jpg")
+                frame = cv2.imread(frame_path, cv2.IMREAD_COLOR) # rgb
+                frames.append(frame)
+
+        team_a_pos, team_b_pos, ball_pos, _ = self.position_arrays[match_number]
+        team_a_pos, team_b_pos = ensure_equal_teamsize(team_a_pos, team_b_pos)
+        assert (team_a_pos.shape == team_b_pos.shape), f"Teams have different shapes: A: {team_a_pos.shape}, B: {team_b_pos.shape}"
+
+        # add team information
+        team_a_indicator = np.zeros((*team_a_pos.shape[:2], 1))
+        team_b_indicator = team_a_indicator + 1
+
+        team_a_pos = np.concatenate([team_a_pos, team_a_indicator], axis=-1)
+        team_b_pos = np.concatenate([team_b_pos, team_b_indicator], axis=-1)
+
+        teams_pos = np.hstack([team_a_pos, team_b_pos])
+
+        ball_pos = ball_pos[:, 0, :] # there is just one ball
+        # add z dim for ball, this should be there later
+        ball_z = np.zeros((ball_pos.shape[0], 1))
+        ball_pos = np.concatenate([ball_pos, ball_z], axis=-1)
+        ball_pos = np.expand_dims(ball_pos, 1)
+
+        all_pos = np.hstack([teams_pos, ball_pos])
+
+        instance = {
+            "frames" : frames,
+            "label" : label, 
+            "positions" : all_pos,
+            "label_offset" : label_offset
+        }
+        return instance
+
+    def export_json(self, idx):
+        """Generate trajectory in json format for internal visualization tool.
+        """
+        positions, *_ = self.__getitem__(idx)
+        # list per frame, inside coordinate lists
+        # positions is T positions, T balls
+        jd = {}
+        team_a = [positions[f*22 : f*22 + 11, :2].tolist() for f in range(self.seq_len)]
+        team_b = [positions[f*22 + 11 : (f+1) * 22, :2].tolist() for f in range(self.seq_len)]
+        
+        jd["team_a"] = team_a
+        jd["team_b"] = team_b
+        jd["balls"] = positions[-self.seq_len:].tolist()
+        return jd
+
+
+def get_index_offset(boundaries, idx2frame, idx):
+    """The dataset is indexed by idx2frame frames and positions based on sequence length - not all frames have positional data.
+    This function maps the index w.r.t. the dataset to the the match (mapping) and the frame number (offset)
+
+    Args:
+        boundaries (List[int]): Dataset indices that belong to the next match
+        idx2frame (List[np.array]): Mapping from idx to frame number per match
+        idx (int): Index wrt. dataset
+
+    Returns:
+        match_number (int): Match number for given index
+        frame_idx (int): Frame number for given index
+    """
+    for match, (i, j) in enumerate(zip(boundaries, boundaries[1:])):
+        if i <= idx and j >= idx:
+            match_number = match
+            offset = idx - boundaries[match]
+            frame_idx = idx2frame[match][offset]
+            return match_number, frame_idx
+    
+
+def ensure_equal_teamsize(team_a, team_b):
+    """Some positional data come with different team sizes.
+    We pad the smaller team with zeros to be able to concatenate the team positions later.
+
+    Args:
+        team_a (np.array): Positional data for the first team.
+        team_b (np.array): Positional data for the second team.
+
+    Returns:
+        team_a (np.array): Padded positional data for the first team.
+        team_b (np.array): Padded positional data for the second team.
+    """
+    if team_a.shape == team_b.shape:
+        return team_a, team_b
+    
+    pad_size = team_a.shape[1] - team_b.shape[1]
+
+    if pad_size < 0: # team b has more players
+        team_a = np.pad(team_a, ((0, 0), (0, -pad_size), (0, 0)), constant_values=0)
+    else:
+        team_b = np.pad(team_b, ((0, 0), (0, pad_size), (0, 0)), constant_values=0)
+
+    return team_a, team_b
+
+if "__main__" == __name__:
+    data = HandballSyncedDataset("/nfs/home/rhotertj/datasets/hbl/meta.csv", 8)
+    
+
