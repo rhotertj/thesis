19e3d478d2c91d5cb2b47797d0e401d82b0f7829
Jim Rhotert <rhotert@stud.uni-hannover.de>
2022-12-05 15:46:18 +0100

    visualization for dataset

---
+++ src/utils.py
@@ -0,0 +1,40 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from data import HandballSyncedDataset
+
+def draw_trajectory(positions : np.array):
+    positions[:, -1, 2] = 2 # give ball an extra team
+    T = positions.shape[0]
+    # start new figure
+    plt.close()
+    fig = plt.figure()
+
+    for t in range(T):
+        a = t / T
+        x_t = positions[t, :, 1]
+        y_t = positions[t, :, 0]
+        teams = positions[t, :, 2]
+        players = list(range(positions.shape[1]))
+        # change axis to improve format in plot
+        sns.scatterplot(y=x_t, x=y_t, hue=teams, style=players, legend=False, palette="Set1", alpha=a)
+
+    return fig 
+
+
+def array2gif(arr : np.array, out_path : str, fps : int):
+    # Expect tensor to be [C, F, H, W]
+    if arr.ndim == 5:
+        arr = arr.squeeze(0)
+
+    duration = (1 / fps) * 1000
+    
+    arr = arr.swapaxes(0,1).astype(np.uint8)
+    frames_arr = list(arr)
+
+    frames = [Image.fromarray(fr.transpose(1,2,0), mode="RGB") for fr in frames_arr]
+    first_frame = frames[0]
+    # Each frame gets duration ms, gif is looped forever (0) (1 is loop once etc)
+    first_frame.save(out_path, format="GIF", append_images=frames, save_all=True, duration=duration, loop=0, interlace=False, includ_color_table=True)
+    print("Saved to", out_path)
