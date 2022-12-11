7d0d6743b587d3d8bfb31e6c366c7898f2221e50
Jim Rhotert <rhotert@stud.uni-hannover.de>
2022-12-11 19:11:36 +0100

    More plots per class

--- src/utils.py
+++ src/utils.py
@@ -4,27 +4,44 @@ import numpy as np
 from PIL import Image
 
 def draw_trajectory(positions : np.ndarray):
-    positions[:, -1, 2] = 2 # give ball an extra team
+    """Plots player and ball positions as a scatterplot.
+    Positions of the past become transparent.
+
+    Args:
+        positions (np.ndarray): Positions of shape [T, N, 3].
+
+    Returns:
+        plt.figure.Figure: The plot.
+    """
     T = positions.shape[0]
-    # start new figure
+    # start new figure, toss the old one
     plt.close()
     fig = plt.figure()
+    # plot config
     plt.xlim(0,40)
     plt.ylim(0,20)
 
+    positions[:, -1, 2] = 2 # give ball an extra team
+
     for t in range(T):
-        a = t / T
+        a = t / T # transparency
         y_t = positions[t, :, 1]
         x_t = positions[t, :, 0]
-        teams = positions[t, :, 2]
-        players = list(range(positions.shape[1]))
-        # change axis to improve format in plot
-        sns.scatterplot(y=y_t, x=x_t, hue=teams, style=players, legend=False, palette="Set1", alpha=a)
+        team_indicator = positions[t, :, 2] # team indicator, used for colors
+
+        sns.scatterplot(y=y_t, x=x_t, hue=team_indicator, legend=False, palette="Set1", alpha=a)
 
     return fig 
 
 
 def array2gif(arr : np.ndarray, out_path : str, fps : int):
+    """Turns a stack of frames into a gif.
+
+    Args:
+        arr (np.ndarray): Stacked RGB frames of shape [1, C, F, H, W]
+        out_path (str): Destination path for the gif.
+        fps (int): Frames per second.
+    """
     # Expect tensor to be [C, F, H, W]
     if arr.ndim == 5:
         arr = arr.squeeze(0)
@@ -36,6 +53,6 @@ def array2gif(arr : np.ndarray, out_path : str, fps : int):
 
     frames = [Image.fromarray(fr.transpose(1,2,0), mode="RGB") for fr in frames_arr]
     first_frame = frames[0]
-    # Each frame gets duration ms, gif is looped forever (0) (1 is loop once etc)
+    # looped forever (0) (1 is loop once etc)
     first_frame.save(out_path, format="GIF", append_images=frames, save_all=True, duration=duration, loop=0, interlace=False, includ_color_table=True)
-    print("Saved to", out_path)
+    
