d2ba345411bc93dbba903811eb9ce80bd7bfa863
Jim Rhotert <rhotert@stud.uni-hannover.de>
2023-03-01 14:08:24 +0100

    more balanced sampling

--- dataset/resample_data.py
+++ dataset/resample_data.py
@@ -10,7 +10,7 @@ from sklearn.model_selection import train_test_split
 
 sys.path.append("/nfs/home/rhotertj/Code/thesis/src")
 from utils import generate_class_description_fine, generate_class_description_coarse
-from data.datasets import MultiModalHblDataset
+from data.datasets import MultiModalHblDataset, ResampledHblDataset
 
 def create_dataframe_from_dataset(dataset : Dataset, path : str = None) -> pd.DataFrame:
     """Creates a `pd.DataFrame` that contains information about frame number, match and label per instance.
@@ -63,11 +63,30 @@ def random_split_dataframe(df : pd.DataFrame, val_size : float = 0.15, test_size
     """    
     train_size = 1 - test_size
     df_idx = df.index.tolist() # serves as X
-
-    train_idx, test_idx, *_ = train_test_split(df_idx, test_size=test_size)
-
-    train_idx, val_idx, *_ = train_test_split(train_idx, test_size=val_size/train_size)
+    y = df["class_coarse"]
+    train_idx, test_idx, *_ = train_test_split(df_idx, stratify=y, test_size=test_size)
+    y = df.loc[train_idx, "class_coarse"]
+    train_idx, val_idx, *_ = train_test_split(train_idx, stratify=y, test_size=val_size/train_size)
     return train_idx, val_idx, test_idx
+
+
+def split_dataframe(df : pd.DataFrame, val_size : float = 0.15, test_size : float = 0.15):
+    """Random splits a `pd.DataFrame` into train, val and test split.
+
+    Args:
+        df (pd.DataFrame): DataFrame to split.
+        val_size (float, optional): Size of the validation split. Defaults to 0.15.
+        test_size (float, optional): Size of the test split. Defaults to 0.15.
+
+    Returns:
+        Tuple[List[int]]: Indices of the splits.
+    """    
+    train_size = 1 - test_size
+    df_idx = df.index.tolist()
+    train_idx, test_idx, *_ = train_test_split(df_idx, shuffle=False, test_size=test_size)
+    train_idx, val_idx, *_ = train_test_split(train_idx, shuffle=False, test_size=val_size/train_size)
+    return train_idx, val_idx, test_idx
+
 
 
 if "__main__" == __name__:
@@ -97,31 +116,47 @@ if "__main__" == __name__:
     overlap_df, non_overlap_df = dfs
 
     overlapped_shots = overlap_df[overlap_df.class_coarse == "Shot"]
-    simple_passes = non_overlap_df[non_overlap_df.class_coarse == "Pass"]
+    simple_passes = overlap_df[overlap_df.class_coarse == "Pass"].iloc[:len(overlapped_shots)]
     simple_background = non_overlap_df[non_overlap_df.shot.isnull()]
     full_df = pd.concat([simple_passes, simple_background, overlapped_shots])
+    full_df.sort_values(by=["frame_idx"], inplace=True)
     full_df.reset_index(inplace=True, drop=True)
 
-    print(full_df.sample(10))
-    print(full_df.value_counts(["class_coarse"]))
-    train_idx, val_idx, test_idx = random_split_dataframe(full_df)
+    # print(full_df.value_counts(["class_coarse"]))
+    train_idx, val_idx, test_idx = split_dataframe(full_df)
+
     train_df = full_df.iloc[train_idx]
     train_df.reset_index(inplace=True, drop=True)
-    print(train_df.value_counts(["class_coarse"]))
-    train_df.to_json(data_path / f"meta3d_{sql}_{sr}_{'overlap' if overlap else 'nooverlap'}_train_balanced.jsonl", lines=True, orient="records")
+    # print(train_df.value_counts(["class_coarse"]))
+    train_df.to_json(data_path / f"meta3d_{sql}_{sr}_train_balanced.jsonl", lines=True, orient="records")
 
     val_df = full_df.iloc[val_idx]
     val_df.reset_index(inplace=True, drop=True)
     print(val_df.value_counts(["class_coarse"]))
-    val_df.to_json(data_path / f"meta3d_{sql}_{sr}_{'overlap' if overlap else 'nooverlap'}_val_balanced.jsonl", lines=True, orient="records")
+    val_df.to_json(data_path / f"meta3d_{sql}_{sr}_val_balanced.jsonl", lines=True, orient="records")
 
 
     test_df = full_df.iloc[test_idx]
     test_df.reset_index(inplace=True, drop=True)
-    print(test_df.value_counts(["class_coarse"]))
-    test_df.to_json(data_path / f"meta3d_{sql}_{sr}_{'overlap' if overlap else 'nooverlap'}_test_balanced.jsonl", lines=True, orient="records")
+    # print(test_df.value_counts(["class_coarse"]))
+    test_df.to_json(data_path / f"meta3d_{sql}_{sr}_test_balanced.jsonl", lines=True, orient="records")
 
 
+    # TODO: Why is the distribution of classes different when iterating over the dataset than in the source dataframe / jsonl?
+    from data.labels import LabelDecoder
+    ld = LabelDecoder(3)
+    dataset = ResampledHblDataset(
+        idx_to_frame=data_path / f"meta3d_{sql}_{sr}_val_balanced.jsonl",
+        meta_path=data_path / "meta3d.csv",
+        seq_len=sql,
+        sampling_rate=sr, 
+        load_frames=False,
+        label_mapping=ld,
+    )
+    print(dataset.get_class_proportions())
 
-
-    
+    for i, instance in tqdm(enumerate(dataset)):
+        assert i == instance["query_idx"]
+        # frame is different, maybe due to window movement?
+        assert val_df.loc[i, "frame_idx"] == instance["frame_idx"], f"{(val_df.loc[i, 'frame_idx'], instance['frame_idx'])}"
+        assert ld(val_df.loc[i, "label"]) == instance["label"], f"{ld(val_df.loc[i, 'label']), instance['label']}"
