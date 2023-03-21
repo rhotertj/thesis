f0f86917e791760dcdfa40ec083411d4fb987201
Jim Rhotert <rhotert@stud.uni-hannover.de>
2023-03-21 15:49:30 +0100

    create model folder

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo "Downloading mvit model (pretrained on Kinetics400)..."
 # curl -o models/mvit_b_16x4_k400.pt https://dl.fbaipublicfiles.com/pyslowfast/model_zoo/mvitv2/pysf_video_models/MViTv2_S_16x4_k400_f302660347.pyth 
+mkdir models
 curl -o models/mvit_b_16x4.pt https://dl.fbaipublicfiles.com/pytorchvideo/model_zoo/kinetics/MVIT_B_16x4.pyth
